e2314a378e2fe0ee852bc9c184d1afee5581f799
prazors <prazors@gmail.com>
2013-03-29 15:24:39 -0500

    got a working code going

--- ListMerge.java
+++ ListMerge.java
@@ -5,6 +5,7 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+import java.util.*;
 public class ListMerge
 {
     public static int[] listMerge(int[] a, int[] b)
@@ -46,34 +47,42 @@ public class ListMerge
             
             if (i < a.length && j < b.length)
             {
-                i++;
-                j++;
+                
+                
                 
                 if (a[i] > b[j])
                 {
                     min = b[j];
-                    
+                    j++;
+                    i++;
                     
                 }
                 else if (a[i] < b[j])
                 {
                     min = a[i];
+                    i++;
+                    j++;
                 }
                 else
                 {
                     min = a[i];
+                    i++;
+                    j++;
                 }
+                
             }
             else if (i < a.length)
             {
-                i++;
+                
                 min = a[i];
+                i++;
                 
             }
             else if (j < b.length)
             {
-                j++;
+                
                 min = b[i];
+                j++;
                 
             }
             
@@ -81,5 +90,26 @@ public class ListMerge
         }
         
         return c;
+    }
+    
+    
+    public static void main(String args[])
+    {
+        
+        
+        
+        int a[] = {23, 17, 4, 54, 9};
+        int b[] = {43, 2, 19, 12};
+        
+        int c[] = listMerge(a, b);
+        
+        for (int i = 0; i < c.length; i++)
+        {
+            System.out.print(c[i] + " ");          
+            
+        }
+        
+        
+        
     }
 }
