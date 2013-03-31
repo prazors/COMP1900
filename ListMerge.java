284cbc93f30c02fc51b06a448c0b7df3f64deb2f
prazors <prazors@gmail.com>
2013-03-31 09:59:36 -0500

    here is a final working copy

--- ListMerge.java
+++ ListMerge.java
@@ -8,6 +8,8 @@
 import java.util.*;
 public class ListMerge
 {
+    
+
     public static int[] listMerge(int[] a, int[] b)
     {
         int i = 0, j = 0, size = 0;
@@ -81,7 +83,7 @@ public class ListMerge
             else if (j < b.length)
             {
                 
-                min = b[i];
+                min = b[j];
                 j++;
                 
             }
@@ -93,22 +95,72 @@ public class ListMerge
     }
     
     
+    
+    //This method takes an array of integers and sorts it from lowest to highest
+    public static int[] intArraySort(int a[])
+    {
+        int i = 0;
+        int j = 1;
+        int k;
+        
+            //this loop will continue to swap variables until swapping is no longer used through a complete traversal of the array
+            while (j < a.length)
+            {
+                //test if swapping is necessary
+                if (a[i] > a[j])
+                {
+                    //swaps the value of a[i] with a[j] so that the higher value is moved back towards the beginning of the array
+                    k = a[i];
+                    a[i] = a[j];
+                    a[j] = k;
+                    
+                    //resets the indices i, j, back to 0 and 1
+                    i = 0;
+                    j = 1;
+                    
+                }
+                //if a swap wasn't needed, then simply increment the indices i, j
+                else
+                {
+                    i++;
+                    j++;
+                }
+                                
+            }       
+        
+        return a;
+        
+        
+    }
+    
     public static void main(String args[])
     {
         
         
         
-        int a[] = {23, 17, 4, 54, 9};
-        int b[] = {43, 2, 19, 12};
+        int a[] = {5, 3, 1, 9, 7};
+        int b[] = {43, 2, 19, 12, 99, 23, 82};
+        
+        a = intArraySort(a);
+        b = intArraySort(b);
         
         int c[] = listMerge(a, b);
         
-        for (int i = 0; i < c.length; i++)
+        int[] d[] = {a, b, c};
+        char mychar = 'a';
+        for (int i = 0; i < 3; i++)
         {
-            System.out.print(c[i] + " ");          
+            System.out.print("List " + mychar + ":\t");
+            for (int j = 0; j < d[i].length; j++)
+            {
+                System.out.print(d[i][j] + "\t");          
+                
+            }
+            System.out.println();
+            mychar++;
+        
             
         }
-        
         
         
     }
